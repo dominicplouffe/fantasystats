75ad499a0c593f12a091a25d23158f14436aa206
Dominic Plouffe <dominic@dplouffe.ca>
2020-07-24 11:31:26 -0400

    fixed a few bugs

--- fantasystats/context.py
+++ fantasystats/context.py
@@ -6,3 +6,7 @@ logger = logging.getLogger(__name__)
 
 db_modules = Database.Database()
 db = db_modules.get_database()
+
+API_URL = 'http://localhost:9500/'
+
+MLB_GAME_API = 'https://statsapi.mlb.com/api/v1.1/game/%s/feed/live'
--- fantasystats/tools/converters.py
+++ fantasystats/tools/converters.py
@@ -19,7 +19,10 @@ class ObjectIDConverter(BaseConverter):
 class JSONEncoder(json.JSONEncoder):
     def default(self, obj):
         if hasattr(obj, 'strftime'):
-            return obj.strftime('%Y-%m-%d')
+            if obj.hour > 0 or obj.minute > 0:
+                return obj.strftime('%Y-%m-%d %H:%M')
+            else:
+                return obj.strftime('%Y-%m-%d')
         if isinstance(obj, ObjectId):
             return str(obj)
         raise TypeError(
